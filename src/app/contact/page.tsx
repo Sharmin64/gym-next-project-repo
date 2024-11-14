8c6d20593b631cc4b08e6929687e7f6f62ea3dc6
Sharmin64 <lizaeatsbanana@gmail.com>
2024-11-14 17:43:43 +0600

    initial setup done

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContactUsPage = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-center">
+        this contact page will be render in hear!!!!!!
+      </h1>
+    </div>
+  );
+};
+
+export default ContactUsPage;
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-center mt-20 ">
+        well come to my fitness gym paga
+      </h1>
+    </div>
+  );
+};
+
+export default HomePage;
